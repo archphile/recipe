8800654fd7dbebfb58c49dccf2f751fe594290f0
archphile <null@mgcloud.eu>
2020-04-17 11:50:21 +0300

    fixed repo

--- scripts/archphile-create-script-rpi4.sh
+++ scripts/archphile-create-script-rpi4.sh
@@ -138,7 +138,7 @@ cat >> /etc/pacman.conf <<"EOF"
 
 [archphile]
 SigLevel = Never
-Server = http://archphile.org/repo/archphile/arm7/corona
+Server = http://archphile.org/repo/archphile/aarch64/corona
 EOF
 
 # Fetching Custom Pacman Mirrorlist
